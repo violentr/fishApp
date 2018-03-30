cd212789f23150702cf13a832cd9bfebe46f9646
Deniss <den.b@itservicesgroup.co.uk>
2018-03-30 18:32:00 +0100

    Add Router Component with app routes

---
+++ src/components/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import StorePicker from "../components/StorePicker";
+import App from "../components/App";
+import NotFound from "../components/NotFound";
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={StorePicker} />
+      <Route path="/store/:storeId" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./components/App";
+import Router from "./components/Router";
 
 import "./style.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-render(<App />, document.getElementById("root"));
+render(<Router />, document.getElementById("root"));
 registerServiceWorker();
