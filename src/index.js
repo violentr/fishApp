85d0417518f8aab59ec7344800c2ee945fc85df8
Deniss <den.b@itservicesgroup.co.uk>
2018-03-30 16:25:56 +0100

    Remove not used file imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+render(<StorePicker />, document.getElementById('root'));
 registerServiceWorker();
