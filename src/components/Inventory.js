79e47f4fe9878ece085403f127bb624c7e7519e3
Deniss <den.b@itservicesgroup.co.uk>
2018-03-30 21:10:51 +0100

    Add Move AddFishForm to Inventory component

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,9 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
   render() {
+    <AddFishForm />;
     return <div className="inventory">Hello</div>;
   }
 }
