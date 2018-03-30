37a6943f1ceacb7510cc45da13a7ffadcdcdef74
Deniss <den.b@itservicesgroup.co.uk>
2018-03-30 18:55:35 +0100

    Add Go to store when button clicked

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = event => {
+    event.preventDefault();
+  };
   render() {
     return (
-      <form action="" className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          ref={this.myInput}
+          placeholder="Store Name"
+        />
         <button type="submit"> Visit Store </button>
       </form>
     );
